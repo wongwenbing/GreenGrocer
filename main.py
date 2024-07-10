e857bd2a50947f0555dcf0919bcedb4cbdd8cf9a
wongwenbing <wongwenbing0718@gmail.com>
2024-07-10 23:20:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,6 @@ from flask import Flask , render_template, request, redirect, url_for
 import mysql.connector
 app = Flask(__name__)
 
-mydb=mysql.connector.connect(
-    host="mysql-1698fa8f-wongwenbing0718-aaf0.e.aivencloud.com",
-    user="avnadmin",
-    password= "AVNS_iBl4eOysp6UaiypUdJd",
-    database="greengrocerdb"
-)
 
 @app.route('/')
 def home(): 
