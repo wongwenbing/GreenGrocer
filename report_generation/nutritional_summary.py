e82c4317090eeee266aa886a613a42e9099b1685
wongwenbing <109064325+wongwenbing@users.noreply.github.com>
2024-07-10 17:41:20 +0800

    Committing

---
+++ report_generation/nutritional_summary.py
@@ -0,0 +1,10 @@
+import mysql.connector
+import pandas as pd 
+import numpy
+
+mydb=mysql.connector.connect(
+    host="mysql-1698fa8f-wongwenbing0718-aaf0.e.aivencloud.com",
+    user="avnadmin",
+    password= "AVNS_iBl4eOysp6UaiypUdJd",
+    database="greengrocerdb"
+)
