7ca42aa5fc102d05ffcd19556fff14267c7fd4f0
wongwenbing <wongwenbing0718@gmail.com>
2024-07-21 00:35:05 +0800

    modifying navbar

--- dataconversion.py
+++ dataconversion.py
@@ -4,17 +4,17 @@ from db import db_connector
 db,cursor = db_connector()
 
 query = "SELECT * FROM Products"
-
 cursor.execute(query)
-
 rows = cursor.fetchall()
+
 
 def to_dataframe(cursor, rows):
     # Get column names from the cursor description
     columns = [desc[0] for desc in cursor.description]
-
     # Convert the fetched data into a DataFrame
     df = pd.DataFrame(rows, columns=columns)
     # Display the DataFrame
-    return df 
+    return df
 
+df = to_dataframe(cursor, rows)
+print(df)
