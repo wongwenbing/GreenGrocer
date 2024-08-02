79d8c297a5500c3c8d734ae87516f697b28c117f
wongwenbing <wongwenbing0718@gmail.com>
2024-08-03 00:17:38 +0800

    custreport updates

--- report_generation/customer_report.py
+++ report_generation/customer_report.py
@@ -3,5 +3,29 @@ import sys
 sys.path.append('../GreenGrocer')
 from db import db_connector
 db, cursor = db_connector()
+import json
 
-class PurchasingReporrt:
+class PurchasingReport:
+    def __init__(self):
+        self.startdate = startdate
+        self.enddate = enddate
+        self.custid = custid
+        self.total = total
+
+    def get_total_amount(self):
+        sum = self.total
+
+    def get_trend_spending(self):
+        query = """
+        SELECT * 
+        FROM ORDERS
+        WHER E"""
+
+    def get_mostpurchased_category(self):
+        query = """
+        SELECT Products
+        """
+
+    def generate_report(self):
+        total = self.get_total_amount.to_json()
+
