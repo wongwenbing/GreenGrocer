45846efb15b03258884b291bbc0dcef5edaa2da4
wongwenbing <wongwenbing0718@gmail.com>
2024-11-24 16:23:48 +0800

    commit

--- db.py
+++ db.py
@@ -7,12 +7,12 @@ def db_connector():
   charset="utf8mb4",
   connect_timeout=timeout,
   cursorclass=pymysql.cursors.DictCursor,
-  db="<insert db name>",
-  host="<insert db host>",
-  password="<insert db pw>",
+  db="greengrocerdb",
+  host="mysql-1698fa8f-wongwenbing0718-aaf0.e.aivencloud.com",
+  password="AVNS_iBl4eOysp6UaiypUdJd",
   read_timeout=timeout,
   port=19222,
-  user="<insert db user>",
+  user="avnadmin",
   write_timeout=timeout,
   )
   
